d444b70f9528f780f360c09c5824d6d0ca4e8dac
davidcoats <david@getsafetycone.com>
2023-12-14 01:34:56 -0800

    Interim changes.

---
+++ source/R5T.T0159/Code/_Types/Interfaces/IHasTextOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0159
+{
+    [DataTypeMarker]
+    public interface IHasTextOutput
+    {
+        public ITextOutput TextOutput { get; }
+    }
+}
